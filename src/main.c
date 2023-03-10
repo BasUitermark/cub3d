03eb3544c671498c75e5be01a75eff2343f6fb33
Bas Uitermark <88148607+BasUitermark@users.noreply.github.com>
2023-03-10 12:35:33 +0100

    fixed raytrace bug

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jde-groo <jde-groo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/01 13:26:25 by jde-groo      #+#    #+#                 */
-/*   Updated: 2023/03/09 19:00:53 by buiterma      ########   odam.nl         */
+/*   Updated: 2023/03/10 12:30:26 by buiterma      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,8 @@ void	test(t_cub3d *cub3d)
 	t_ray	ray;
 	int		index;
 
-	ft_memset(cub3d->foreground->pixels, 0, cub3d->foreground->width \
-		* cub3d->foreground->height * sizeof(int));
+	mlx_delete_image(cub3d->mlx, cub3d->foreground);
+	cub3d->foreground = mlx_new_image(cub3d->mlx, WIDTH, HEIGHT);
 	index = 0;
 	while (index < WIDTH)
 	{
@@ -56,8 +56,7 @@ void	test(t_cub3d *cub3d)
 		draw_line(&ray, cub3d->foreground, cub3d->textures[ray.side], index);
 		index++;
 	}
-	// moving this bitch to main seems to induce less lag
-	// mlx_image_to_window(cub3d->mlx, cub3d->foreground, 0, 0); // this shit is causing lag ????? how ??????
+	mlx_image_to_window(cub3d->mlx, cub3d->foreground, 0, 0);
 }
 
 int	main(const int argc, const char *argv[])
@@ -76,8 +75,7 @@ int	main(const int argc, const char *argv[])
 	cub3d.movSpeed = 0.045;
 	cub3d.rotSpeed = 0.05;
 	mlx_image_to_window(cub3d.mlx, cub3d.background, 0, 0);
-	mlx_image_to_window(cub3d.mlx, cub3d.foreground, 0, 0);
-	mlx_loop_hook(cub3d.mlx, &new_move, &cub3d);	// (not sure!!!) uses queue which gets really, really big since events constantly fire and it cannot keep up (i think!!)
+	mlx_loop_hook(cub3d.mlx, &new_move, &cub3d);
 	mlx_loop(cub3d.mlx);
 	return (cleanup(&cub3d, EXIT_SUCCESS));
 }
